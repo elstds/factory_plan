bb66a22a1830495a2d5756dd2dbad6673f9e9884
local <local@MAINFRAME>
2023-12-18 16:38:29 +0500

    Добавьте файлы проекта.

---
+++ Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace WpfApp1
+{
+    class Database
+    {
+        SqlConnection _connection = new SqlConnection(@"Data Source=MAINFRAME\IOMLSQLSRV; Initial Catalog=factory");
+        public void openConnection()
+        {
+            if(_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return _connection;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Database factory_db = new Database();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(AccessCombobox.SelectedIndex == 1 || AccessCombobox.SelectedIndex == 2) {
+                UserNameTextbox.IsEnabled = true;
+                PassBox.IsEnabled = true;
+            } 
+            if (AccessCombobox.SelectedIndex == 0)
+            {
+                UserNameTextbox.IsEnabled = false;
+                PassBox.IsEnabled = false;
+            }
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AccessCombobox.SelectedIndex == 1 || AccessCombobox.SelectedIndex == 2) {
+                int access_level = AccessCombobox.SelectedIndex;
+                string username = UserNameTextbox.Text;
+                string password = PassBox.Password;
+                if (access_level == 2 &&  username =="admin" && password == "admin")
+                {
+                    Admin admin_window = new Admin();
+                    admin_window.Show();
+                    Hide();
+                    
+                } else if (access_level == 1 &&  username =="loader" && password == "1234") {
+                    Loader loader_window = new Loader();
+                    loader_window.Show();
+                    Hide();
+                } 
+            }
+            else if (AccessCombobox.SelectedIndex == 0)
+            {
+                View view_window = new View();
+                view_window.Show();
+                Hide();
+            }
+        }
+    }
+}
